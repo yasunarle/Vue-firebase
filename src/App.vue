64a65ff903295d2bacd26b5b64d0ef9c25553bec
Yasunari t <u7df25c588m737@gmail.com>
2020-09-10 19:16:36 +0900

    chore: created shun page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
-    <div class="header">      
-      <router-link to="/">Home</router-link> 
-      <router-link to="/database">DB</router-link> 
+    <div class="header">
+      <router-link to="/">Home</router-link>
+      <router-link to="/database">DB</router-link>
       <!-- header if log in...  -->
-      <div v-if = "user.uid" class = "login">
-        <a @click="doLogout">LogOut</a>  
+      <div v-if="user.uid" class="login">
+        <a @click="doLogout">LogOut</a>
       </div>
-      <div v-else class = "not_log_in">
+      <div v-else class="not_log_in">
         <router-link to="/signin">Login</router-link>
         <router-link to="/signup">Signup</router-link>
       </div>
     </div>
     <!-- Router -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 
 <script>
 // firebase モジュール
-import firebase from 'firebase'
+import firebase from "firebase";
 // import func from '../vue-temp/vue-editor-bridge';
 // import axios from 'axios'
 // import FileReader from 'filereader'
@@ -28,59 +28,57 @@ import firebase from 'firebase'
 export default {
   data() {
     return {
-      user: {},  // ユーザー情報
-      identimal_name: '',  // 入力したメッセージ
-      selectedFile: '',
+      user: {}, // ユーザー情報
+      identimal_name: "", // 入力したメッセージ
+      selectedFile: "",
       // image: '',
-      imageName: 'identimal１号機',
-      imageUrl: '',
+      imageName: "identimal１号機",
+      imageUrl: "",
       image: null,
-      key: 'testzkoetnafoejraa',
-      ext: ''
-
-    }
+      key: "testzkoetnafoejraa",
+      ext: "",
+    };
   },
   created() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.user = user ? user : {}
-    })
+    firebase.auth().onAuthStateChanged((user) => {
+      this.user = user ? user : {};
+    });
   },
   methods: {
     doLogout() {
-      firebase.auth().signOut()      
-    },    
-  }
-} 
+      firebase.auth().signOut();
+    },
+  },
+};
 </script>  
 
 <style>
-body{
+body {
   margin: 0;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-.header {  
+.header {
   padding: 10px;
   background: #5c5f6e;
-  a {    
+  /* a {
     font-weight: bold;
-    color: #2c3e50;   
-  }
+    color: #2c3e50;
+  } */
 }
 .router-link-exact-active {
   color: #42b983;
 }
-.text_form{
+.text_form {
   border: 2px solid #42b983;
   width: 400px;
   height: 200px;
   margin: auto;
 }
-
 </style>
 
--- src/views/Shun.vue
+++ src/views/Shun.vue
@@ -1,42 +1,51 @@
 <template>
   <div class="shun-container">
-    <div class="inner-width">
-      <p v-for="(item, index) in items" :key="index" class="p">
-        {{ item.name }}
-      </p>
-      <input type="text" v-model="inputText" v-on:keyup.enter="sendStore" placeholder="Name">
-    </div>    
+    <h1>Life Design</h1>
+    <div class="life-design">
+      <div class="life-design__content">
+        <div class="age">1997</div>
+        <h1>test</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { db } from '@/main'
 export default {
-  data(){
+  data() {
     return {
-      inputText:"",
-      items:{}
-    }
+      user: {
+        name: "Yasunari Tanka",
+        description: "よろしくお願いします。",
+        lifedesign: [],
+      },
+    };
   },
-  // firestore:{
-  //   items: db.collection('items')
-  // },  
-  methods:{
-    sendStore(){
-      db.collection('items').add({name: this.inputText})
-      this.inputText = ""
-    }
-  }
-}
+};
 </script>
 
 <style>
-.shun-container .inner-width{
+.shun-container .inner-width {
   max-width: 800px;
   margin: auto;
   padding-top: 100px;
 }
-.shun-container .p{
-  border-bottom:1px solid black; 
-}  
+.life-design {
+  height: 600px;
+  max-width: 900px;
+  margin: auto;
+  padding: 10px;
+  background: grey;
+  overflow: auto;
+}
+.life-design__content {
+  width: 10000px;
+  height: 100%;
+  background-color: whitesmoke;
+  color: black;
+}
+.age {
+  background: yellow;
+  color: black !important;
+}
 </style>
